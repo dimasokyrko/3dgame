59d1ccc03d6b41f834a9c4f1a5c828f6984f5e25
dimasokyrko <dima24sokyrko@gmail.com>
2021-03-30 23:00:25 +0300

    Initial commit

---
+++ Assets/Scripts/LivingCreature.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Collider), typeof(Rigidbody), typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public abstract class LivingCreature : MonoBehaviour
+{
+    public Rigidbody CreatureRB { get; private set; }
+    public Collider CreatureCollider { get; private set; }
+    public NavMeshAgent CreatureNavMeshAgent { get; private set; }
+    public Animator CreatureAnimator { get; private set; }
+    public ServiceManager ServiceManager { get; private set; }
+    public LivingCreatureActionController ActionController { get; protected set; }
+
+    protected virtual void Start()
+    {
+        CreatureRB = GetComponent<Rigidbody>();
+        CreatureCollider = GetComponent<Collider>();
+        CreatureNavMeshAgent = GetComponent<NavMeshAgent>();
+        CreatureAnimator = GetComponent<Animator>();
+        ServiceManager = ServiceManager.Instance;
+    }
+}
